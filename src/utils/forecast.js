eb08f0766a93e1d3bb56f9659c1fb476ddeb9710
salman <sama020@gmail.com>
2020-05-13 11:29:04 +0300

    added some data on forcast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It fells like ' + body.current.feelslike + ' degress out and the humidity is ' + body.current.humidity
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + 
+            ' degress out. It fells like ' + body.current.feelslike + ' degress out. The humidity is ' + body.current.humidity + '%.'
             )
         }
     })
